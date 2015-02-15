5210510d89474d212f2d6c621c083d5dd38490c0
Jeff Posnick <jeffy@google.com>
2015-02-14 21:21:47 -0500

    Gulp changes.

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,21 @@ var bower = require('gulp-bower');
 var browserSync = require('browser-sync');
 var gulp = require('gulp');
 var path = require('path');
+var runSequence = require('run-sequence');
+var spawn = require('child_process').spawn;
 
 var DEV_DIR = 'dev/';
 
 gulp.task('bower', function() {
   return bower({
     cwd: DEV_DIR
+  });
+});
+
+gulp.task('npm-install', function(callback) {
+  var backend = spawn('npm', ['install']);
+  backend.on('exit', function(code) {
+    callback(code === 0 ? null : 'Error status from spawned process: ' + code);
   });
 });
 
@@ -20,4 +29,13 @@ gulp.task('serve:dev', function() {
   });
 });
 
-gulp.task('default', ['bower', 'serve:dev']);
+gulp.task('backend', function(callback) {
+  var backend = spawn('node', ['server.js'], {stdio: 'inherit'});
+  backend.on('exit', function(code) {
+    callback(code === 0 ? null : 'Error status from spawned process: ' + code);
+  });
+});
+
+gulp.task('default', function() {
+  runSequence(['npm-install', 'bower'], ['serve:dev', 'backend']);
+});
