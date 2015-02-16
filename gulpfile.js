e370a6ee2f821ad29b30a034aab2f8912477aa50
Jeff Posnick <jeffy@google.com>
2015-02-15 23:37:30 -0500

    Gulp stuff.

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var browserSync = require('browser-sync');
 var browserify = require('browserify');
 var buffer = require('vinyl-buffer');
 var del = require('del');
+var ghPages = require('gulp-gh-pages');
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var runSequence = require('run-sequence');
@@ -43,7 +44,7 @@ gulp.task('npm-install', function(callback) {
   });
 });
 
-gulp.task('serve:dev', ['js'], function() {
+gulp.task('serve-frontend', ['js'], function() {
   browserSync({
     server: {
       baseDir: DEV_DIR
@@ -57,12 +58,14 @@ gulp.task('serve:dev', ['js'], function() {
   gulp.watch(DEV_DIR + 'bower.json', ['bower']);
 });
 
-gulp.task('backend', function(callback) {
+gulp.task('serve-backend', function(callback) {
   var backend = spawn('node', ['server.js'], {stdio: 'inherit'});
   backend.on('exit', function(code) {
     callback(code === 0 ? null : 'Error status from spawned process: ' + code);
   });
 });
+
+gulp.task('serve', ['serve-frontend', 'serve-backend']);
 
 gulp.task('vulcanize', function() {
   return gulp.src(DEV_DIR + 'elements.html')
@@ -91,6 +94,10 @@ gulp.task('build', function(callback) {
   runSequence('clean', ['vulcanize', 'js'], 'copy-assets', callback);
 });
 
+gulp.task('gh-pages', ['build'], function() {
+  return gulp.src(DIST_DIR + '**/*').pipe(ghPages());
+});
+
 gulp.task('default', function() {
-  runSequence(['npm-install', 'bower'], ['serve:dev', 'backend']);
+  runSequence(['npm-install', 'bower'], 'serve');
 });
